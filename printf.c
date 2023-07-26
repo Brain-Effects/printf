6d636700247fd4f0ed694ccac44ada2f4934739b
--add <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@github.com>
2023-07-25 21:14:37 -0700

    I'm not going anywhere. You can print that wherever you want to. I'm here and I'm a Spur for life

--- printf.c
+++ printf.c
@@ -3,50 +3,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "main.h"
-
-/**
- * handle_conversion_specifier - handles conversion specifiers
- * @c: the current character
- * @args: the variable argument list
- * @count: a pointer to the count variable
- *
- * Description: This function handles the different conversion specifiers
- * for the _printf function. It updates the count variable to keep track
- * of the number of characters printed.
- */
-void handle_conversion_specifier(char c, va_list args, int *count)
-{
-	switch (c)
-	{
-		case 'c':
-			*count += handle_c(args);
-			break;
-		case 's':
-			*count += handle_s(args);
-			break;
-		case '%':
-			*count += handle_percent();
-			break;
-		case 'd':
-		case 'i':
-			*count += handle_d_i(args);
-			break;
-		case 'b':
-			*count += handle_b(args);
-			break;
-		case 'u':
-			*count += handle_u(args);
-			break;
-		case 'o':
-			*count += handle_o(args);
-			break;
-		case 'x':
-		case 'X':
-			*count += handle_x_X(c, args);
-			break;
-	}
-}
-
 /**
  * _printf - produces output according to a format
  * @format: character string composed of zero or more directives
@@ -60,27 +16,58 @@ void handle_conversion_specifier(char c, va_list args, int *count)
  * to end output to strings)
  */
 int _printf(const char *format, ...)
-{	va_list args;
+{
+	va_list args;
 	int count = 0;
 	int i;
+	char c;
 
 	va_start(args, format);
-
 	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] == '%')
 	{
-		i++;
-		handle_conversion_specifier(format[i], args, &count);
+	i++;
+	c = format[i];
+	switch (format[i])
+	{
+		case 'c':
+			count += handle_c(args);
+			break;
+		case 's':
+			count += handle_s(args);
+			break;
+		case '%':
+			count += handle_percent();
+			break;
+		case 'd':
+		case 'i':
+			count += handle_d_i(args);
+			break;
+		case 'b':
+			count += handle_b(args);
+			break;
+		case 'u':
+			count += handle_u(args);
+			break;
+		case 'o':
+			count += handle_o(args);
+			break;
+		case 'x':
+		case 'X':
+			count += handle_x_X(c, args);
+			break;
+		}
 	}
 		else
-			count += write(1, &format[i], 1);
+		count += write(1, &format[i], 1);
 	}
 
 	va_end(args);
 
 	return (count);
 }
+
 /**
  * handle_c - handles the c conversion specifier
  * @args: variable argument list
@@ -96,6 +83,7 @@ int handle_c(va_list args)
 
 	return (write(1, &c, 1));
 }
+
 /**
  * handle_s - handles the s conversion specifier
  * @args: variable argument list
@@ -111,10 +99,24 @@ int handle_s(va_list args)
 	char *str = va_arg(args, char *);
 
 	while (*str)
-		count += write(1, str++, 1);
+	count += write(1, str++, 1);
 
 	return (count);
 }
+
+/**
+ * handle_percent - handles the % conversion specifier
+ *
+ * Description: This function writes a % character to the standard
+ * output stream.
+ *
+ * Return: the number of characters written
+ */
+int handle_percent(void)
+{
+	return (write(1, "%", 1));
+}
+
 /**
  * handle_d_i - handles the d and i conversion specifiers
  * @args: variable argument list
